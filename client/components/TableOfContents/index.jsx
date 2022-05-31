a49ae594d9a10c67496b73729b676ceb9c9d8f4e
lev <levtrifonov@mail.ru>
2022-06-01 00:24:31 +0500

    убрали хук для проверки размера экрана

--- client/components/TableOfContents/index.jsx
+++ client/components/TableOfContents/index.jsx
@@ -1,11 +1,11 @@
 import React, {useState, useEffect} from 'react';
 import { useRouter } from 'next/router'
 import Link from 'next/link';
-import useMatchMedia from 'use-match-media-hook';
+// import useMatchMedia from 'use-match-media-hook';
 import {getAllPage} from '../../api/apiPage';
 import styles from './TableOfContents.module.css';
 
-const queries = ['(max-width: 400px)', '(min-width: 800px)'];
+// const queries = ['(max-width: 400px)', '(min-width: 800px)'];
 
 function TableOfContents({tableOfContentArr, currentPageUrl = [], anchorLinks}) {
   const router = useRouter()
@@ -17,16 +17,16 @@ function TableOfContents({tableOfContentArr, currentPageUrl = [], anchorLinks}) 
     console.log('текущий урл', currentPageUrl);
   }, [currentPageUrl])
 
-  const [mobile, desktop] = useMatchMedia(queries);
-  let baseState;
+  // const [mobile, desktop] = useMatchMedia(queries);
+  // let baseState;
 
-  if (desktop) {
-    baseState = false;
-  } else {
-    baseState = true;
-  }
+  // if (desktop) {
+  //   baseState = false;
+  // } else {
+  //   baseState = true;
+  // }
 
-  const [isOpen, setIsOpen] = useState(baseState);
+  const [isOpen, setIsOpen] = useState(true);
 
   // TODO: Сделать для большой вложенности...
   const tableOfContentsLink = ({url, title, children}) => (
@@ -51,7 +51,7 @@ function TableOfContents({tableOfContentArr, currentPageUrl = [], anchorLinks}) 
     <>
       <button type="button" className={styles.openButton} onClick={() => setIsOpen(!isOpen)} />
       <nav
-        style={{display: mobile || !isOpen ? 'none' : 'block'}}
+        style={{display: !isOpen ? 'none' : 'block'}}
         className={styles.tableOfContents}
       >
         <ul>
