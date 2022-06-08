169b6e9eba63197d0f84b0356c77c4dd76161e19
Vlad Atkishkin <vladatkishkin@vk.com>
2022-06-08 18:28:27 +0500

    Добавил кнопку перехода на главную страницу Дизайн Кода + починил управление с клавиатуры для домашней страницы

--- client/components/TableOfContents/index.jsx
+++ client/components/TableOfContents/index.jsx
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import { useRouter } from 'next/router'
+import {useRouter} from 'next/router';
 import Link from 'next/link';
 import {getAllPage} from '../../api/apiPage';
 import styles from './TableOfContents.module.css';
@@ -7,14 +7,14 @@ import styles from './TableOfContents.module.css';
 // const queries = ['(max-width: 400px)', '(min-width: 800px)'];
 
 function TableOfContents({tableOfContentArr, currentPageUrl = [], anchorLinks}) {
-  const router = useRouter()
+  const router = useRouter();
 
   useEffect(() => {
     console.log('якорные ссылки', anchorLinks);
-  }, [anchorLinks])
+  }, [anchorLinks]);
   useEffect(() => {
     console.log('текущий урл', currentPageUrl);
-  }, [currentPageUrl])
+  }, [currentPageUrl]);
 
   // const [mobile, desktop] = useMatchMedia(queries);
   // let baseState;
@@ -41,25 +41,27 @@ function TableOfContents({tableOfContentArr, currentPageUrl = [], anchorLinks}) 
           {title}
         </a>
       </Link>
-      {currentPageUrl[1] && currentPageUrl[1] === url && anchorLinks.map((anchor) => (
-        <a className={styles.innerTableOfContentsLink} key={anchor.title} href={`#${anchor.id}`}>
-          {anchor.title}
-        </a>
-      ))}
+      {currentPageUrl[1]
+        && currentPageUrl[1] === url
+        && anchorLinks.map((anchor) => (
+          <a className={styles.innerTableOfContentsLink} key={anchor.title} href={`#${anchor.id}`}>
+            {anchor.title}
+          </a>
+        ))}
     </>
   );
 
   return (
     <>
       <button type="button" className={styles.openButton} onClick={() => setIsOpen(!isOpen)} />
-      <nav
-        style={{display: !isOpen ? 'none' : 'block'}}
-        className={styles.tableOfContents}
-      >
-        <ul>
-          {currentPageUrl
-            && tableOfContentArr.map((obj) => tableOfContentsLink(obj))}
-        </ul>
+      <nav style={{display: !isOpen ? 'none' : 'block'}} className={styles.tableOfContents}>
+        <ul>{currentPageUrl && tableOfContentArr.map((obj) => tableOfContentsLink(obj))}</ul>
+        <a className={styles.logoContainer} href="https://ekaterinburg.design/">
+          {/* <img src="../../public/assets/avatar.png" alt="Дизайн код логотип" /> */}
+          <p className={styles.logoContainerText}>
+            Дизайн-код Екатеринбурга
+          </p>
+        </a>
       </nav>
     </>
   );
--- client/next.config.js
+++ client/next.config.js
@@ -2,8 +2,10 @@ module.exports = {
   api: {
     HOST: 'https://guides-api-test.ekaterinburg.design',
   },
+  images: {
+    domains: ['guides-api-test.ekaterinburg.design'],
+  },
   async rewrites() {
-    console.log('реврайтс');
     return [
       {
         source: '/:pageUrl*',
