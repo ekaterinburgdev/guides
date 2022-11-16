83b860cf81cbf5df6ab6c6ea5f87ff48ab2b3a39
KotikVacia <vladatkishkin@vk.com>
2022-11-17 01:01:06 +0500

    update-typography

--- components/CommonLinks/CommonLinks.jsx
+++ components/CommonLinks/CommonLinks.jsx
@@ -1,13 +1,30 @@
-import Link from "next/link";
+import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
 
-import styles from "./CommonLinks.module.css";
+import styles from './CommonLinks.module.css'
 
-export const CommonLinks = () => {
+export function CommonLinks() {
     return (
         <section className={styles.CommonLinks__container}>
-            <Link className={styles.CommonLinks__link} href="/">
-                Городские руководства
-            </Link>
+            <Image
+                src="/transport.svg"
+                width={60}
+                height={60}
+                className={styles.CommonLinks__icon}
+            />
+            <Image
+                src="/ecosystem.svg"
+                width={60}
+                height={60}
+                className={styles.CommonLinks__icon}
+            />
+            <p>
+                <Link className={styles.CommonLinks__link} href="/">
+                    Руководства <br />
+                    Екатеринбурга
+                </Link>
+            </p>
         </section>
     )
 }
