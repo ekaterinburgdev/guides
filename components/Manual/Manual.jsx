dafbce75bb1851717add889e32deb2d954158cb0
Alexander Chabin <nibach@ya.ru>
2022-12-03 00:47:33 +0500

    Update manual preview design

--- components/Manual/Manual.jsx
+++ components/Manual/Manual.jsx
@@ -1,14 +1,26 @@
 import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
+import cn from 'classnames'
+
 import styles from './manual.module.css'
+
 import { API_HOST } from '../../consts/endpoints'
 
 function Manual({ manual }) {
-    const imageUrl = manual.cover
+    // TODO remove
+    // const imageUrl = manual.cover
     const titleArr = manual?.properties?.Name?.title
     const titleText = titleArr.length > 0 ? titleArr[0]?.text?.content : ''
     const pageUrl = manual?.properties?.pageUrl?.url
+    // TODO check color
+    const color = manual?.properties?.color || '#44a20d'
+    // TODO Add new fields
+    // const status = manual?.properties?.status
+    // const publishedDate
+    // const previewPattern = manual?.properties?.previewPattern
+
+    console.log(API_HOST)
 
     return (
         <Link
@@ -16,15 +28,25 @@ function Manual({ manual }) {
                 pathname: '/[[...pageUrl]]',
                 query: { pageUrl: [pageUrl] },
             }}
-            className={styles.manualItem}
+            className={styles.manual}
         >
-            <Image
-                src={`${API_HOST}/static/${imageUrl}`}
-                alt={titleText}
-                layout="fill"
-                overflow="visible"
-                loading="lazy"
-            />
+            <div className={styles.manualInner} style={{ color }}>
+                <div className={styles.manualTitle}>{titleText}</div>
+
+                {/* <div className={cn(styles.manualStatus, styles.manualStatusUpdated)}>
+                    Обновилось
+                </div> */}
+
+                <div className={cn(styles.manualStatus, styles.manualStatusNew)}>Новое!</div>
+
+                <div className={styles.manualDate}>12.02.2022</div>
+
+                <Image
+                    src="https://svgshare.com/getbyhash/sha1-EBeV3z7BzWIsTwfZjwWo3FzcOWo="
+                    alt=""
+                    layout="fill"
+                />
+            </div>
         </Link>
     )
 }
