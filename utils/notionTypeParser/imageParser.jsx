c627a5bb0c1b9b229784044fc739eed44ef1852e
KotikVacia <vladatkishkin@vk.com>
2022-12-04 19:46:52 +0500

    update-typography: rebase

--- utils/notionTypeParser/imageParser.jsx
+++ utils/notionTypeParser/imageParser.jsx
@@ -2,34 +2,24 @@ import React from 'react'
 import Image from 'next/image'
 import classNames from 'classnames'
 
+import { API_HOST } from '../../consts/endpoints'
 import styles from '../../components/ManualPage/ManualPage.module.css'
 
 const getImage = (imageObj) => {
     const cn = classNames(styles.Manual__image, {
         [styles.Manual__image_svg]: String(imageObj.content.image_name).includes('svg'),
     })
+    const image = (
+        <Image className={cn} src={`${API_HOST}/static/${imageObj.content.image_name}`} fill />
+    )
 
     if (imageObj.content.image_data.caption.length === 0) {
-        return (
-            <div className={styles.Manual__image__container}>
-                <Image
-                    className={cn}
-                    src={`${process.env.HOST}/static/${imageObj.content.image_name}`}
-                    alt=""
-                    fill
-                />
-            </div>
-        )
+        return <div className={styles.Manual__image__container}>{image}</div>
     }
-    
+
     return (
         <div className={styles.Manual__image__container}>
-            <Image
-                className={styles.Manual__image}
-                src={`${process.env.HOST}/static/${imageObj.content.image_name}`}
-                alt={imageObj.content.image_data.caption[0].plain_text}
-                fill
-            />
+            {image}
             <span className={styles.Manual__image_description} aria-hidden="true">
                 {imageObj.content.image_data.caption[0].plain_text}
             </span>
