18bd14de8ab8f9cd2f7a937b26a1a3bf5a7bc892
KotikVacia <vladatkishkin@vk.com>
2022-12-13 01:47:16 +0500

    update-typography: wip

--- utils/getBackgroundColor.js
+++ utils/getBackgroundColor.js
@@ -1,7 +1,7 @@
 import Color from 'color'
 
 function getBackgroundColor(primaryColor) {
-    return Color(primaryColor).alpha(0.2).lighten(0.2).rgb()
+    return Color(primaryColor).alpha(0.8).lighten(0.6).rgb()
 }
 
 export default getBackgroundColor
