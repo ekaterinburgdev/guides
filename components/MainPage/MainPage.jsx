e9428f9807df61ac9595125dc8e039f56247cb86
Alexander Chabin <nibach@ya.ru>
2022-12-19 08:36:22 +0500

    Add new logo to `<MainPage>`

---
+++ components/MainPage/MainPage.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import Image from 'next/image'
+import ManualPreview from '../ManualPreview/ManualPreview'
+
+import mainPageLogo from './MainPageLogo.svg'
+import styles from './MainPage.module.css'
+
+export default function MainPage({ pageTitle, manualsPreview }) {
+    return (
+        <main className={styles.mainPage}>
+            <h1 className={styles.mainPageTitle}>
+                <div className={styles.mainPageLogo}>
+                    <Image src={mainPageLogo} fill alt="" />
+                </div>
+                {pageTitle}
+            </h1>
+
+            <section className={styles.mainPageManuals}>
+                {manualsPreview.map((manualPreviewData) => (
+                    <ManualPreview {...manualPreviewData} key={manualPreviewData.title} />
+                ))}
+            </section>
+        </main>
+    )
+}
