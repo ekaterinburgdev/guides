eda030e1bbddf5fe93890f35a322763cfc76fead
KotikVacia <vladatkishkin@vk.com>
2022-12-31 15:22:09 +0500

    Happy New Year (part 2)

--- components/Snowflake/Showflakes.jsx
+++ components/Snowflake/Showflakes.jsx
@@ -4,7 +4,7 @@ import styles from './Snowflake.module.css'
 
 export default function Showflakes() {
     return (
-        <div className="snowflakes" aria-hidden="true">
+        <div aria-hidden="true">
             <div className={styles.snowflake}>❅</div>
             <div className={styles.snowflake}>❅</div>
             <div className={styles.snowflake}>❆</div>
