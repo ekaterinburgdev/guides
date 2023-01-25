aedc5c50fa76a570e7005c5bcfac28065995a2ef
AngyBlacwood <saldina_2011@mail.ru>
2023-01-25 22:09:00 +0500

    add images zoom and pan

--- utils/notionTypeParser/imageParser.jsx
+++ utils/notionTypeParser/imageParser.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import Image from 'next/image'
 import classNames from 'classnames'
+import Lightbox from 'yet-another-react-lightbox'
+import Zoom from 'yet-another-react-lightbox/plugins/zoom'
+import 'yet-another-react-lightbox/styles.css'
 
 import { API_HOST } from '../../consts/endpoints'
 import styles from '../../components/ManualPage/ManualPage.module.css'
@@ -10,13 +13,48 @@ function GuideImage({ notionType }) {
         [styles.Manual__image_svg]: String(notionType.content.image_name).includes('svg'),
     })
 
+    const [open, setOpen] = React.useState(false)
+    const [animationDuration, setAnimationDuration] = React.useState(500)
+    const [maxZoomPixelRatio, setMaxZoomPixelRatio] = React.useState(1)
+    const [zoomInMultiplier, setZoomInMultiplier] = React.useState(2)
+    const [doubleTapDelay, setDoubleTapDelay] = React.useState(300)
+    const [doubleClickDelay, setDoubleClickDelay] = React.useState(300)
+    const [doubleClickMaxStops, setDoubleClickMaxStops] = React.useState(2)
+    const [keyboardMoveDistance, setKeyboardMoveDistance] = React.useState(50)
+    const [wheelZoomDistanceFactor, setWheelZoomDistanceFactor] = React.useState(100)
+    const [pinchZoomDistanceFactor, setPinchZoomDistanceFactor] = React.useState(100)
+    const [scrollToZoom, setScrollToZoom] = React.useState(false)
+    const [finite, setFinite] = React.useState(true)
+    const src_url = `${API_HOST}/static/${notionType.content.image_name}`
+
     const image = (
         <>
-            <Image
-                className={cn}
-                src={`${API_HOST}/static/${notionType.content.image_name}`}
-                fill
+            <Lightbox
+                open={open}
+                close={() => setOpen(false)}
+                slides={[{ src: src_url }]}
+                plugins={[Zoom]}
+                animation={{ zoom: animationDuration }}
+                zoom={{
+                    maxZoomPixelRatio,
+                    zoomInMultiplier,
+                    doubleTapDelay,
+                    doubleClickDelay,
+                    doubleClickMaxStops,
+                    keyboardMoveDistance,
+                    wheelZoomDistanceFactor,
+                    pinchZoomDistanceFactor,
+                    scrollToZoom,
+                }}
+                carousel={{
+                    finite,
+                }}
+                render={{
+                    buttonPrev: () => null,
+                    buttonNext: () => null,
+                }}
             />
+            <Image onClick={() => setOpen(true)} className={cn} src={src_url} fill />
         </>
     )
 
