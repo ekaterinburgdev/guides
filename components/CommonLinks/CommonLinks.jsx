52533a2fed9509570b354794399a8b901fa4542e
KotikVacia <vladatkishkin@vk.com>
2023-01-25 15:50:27 +0500

    fix-open: try fix open (part 2)

--- components/CommonLinks/CommonLinks.jsx
+++ components/CommonLinks/CommonLinks.jsx
@@ -3,14 +3,15 @@ import Link from 'next/link'
 import Image from 'next/image'
 import rgbaToRgb from 'rgba-to-rgb'
 import classNames from 'classnames'
+import Router from 'next/router'
 
 import styles from './CommonLinks.module.css'
-import { ThemeContext, OpenTocContext } from '../../pages/_app'
+import { ThemeContext } from '../../pages/_app'
 import { useMobileDetect } from '../../hooks/hooks'
 
 export function CommonLinks({ color, bgColor, mainPage }) {
     const isDark = useContext(ThemeContext)
-    const { isOpen } = useContext(OpenTocContext)
+    const { asPath } = useRouter()
     const mainPageColor = isDark ? '#1A1C1F' : '#f5f8fb'
     const commonColor = mainPage
         ? mainPageColor
@@ -27,51 +28,58 @@ export function CommonLinks({ color, bgColor, mainPage }) {
     })
 
     return (
-        <section style={{ backgroundColor: commonColor }} className={containerClassnames}>
-            <Link href="https://ekaterinburg.design/">
-                <Image
-                    src="/ecosystem.svg"
-                    width={60}
-                    height={60}
-                    className={styles.CommonLinks__icon}
-                    style={{ marginTop: '15px' }}
-                />
-            </Link>
-            {mainPage ? (
-                <>
+        asPath !== '/' &&
+        useMobileDetect().isMobile() && (
+            <section style={{ backgroundColor: commonColor }} className={containerClassnames}>
+                <Link href="https://ekaterinburg.design/">
                     <Image
-                        src="/transport.svg"
+                        src="/ecosystem.svg"
                         width={60}
                         height={60}
                         className={styles.CommonLinks__icon}
-                        style={{ marginTop: '14px', marginLeft: '12px' }}
+                        style={{ marginTop: '15px' }}
                     />
-                    <p
-                        className={styles.CommonLinks__text_main}
-                        style={{ color, paddingLeft: '8px' }}
-                    >
-                        Руководства <br />
-                        Екатеринбурга
-                    </p>
-                </>
-            ) : (
-                <Link style={{ paddingLeft: '12px' }} className={styles.CommonLinks__link} href="/">
-                    <Image
-                        src="/transport.svg"
-                        width={60}
-                        height={60}
-                        className={styles.CommonLinks__icon}
-                        style={{ marginLeft: '0', marginTop: '14px' }}
-                    />
-                    <p
-                        className={styles.CommonLinks__text}
-                        style={{ color, textDecorationColor: bgColor.alpha(0.2).rgb() }}
-                    >
-                        Руководства <br />
-                        Екатеринбурга
-                    </p>
                 </Link>
-            )}
-        </section>
+                {mainPage ? (
+                    <>
+                        <Image
+                            src="/transport.svg"
+                            width={60}
+                            height={60}
+                            className={styles.CommonLinks__icon}
+                            style={{ marginTop: '14px', marginLeft: '12px' }}
+                        />
+                        <p
+                            className={styles.CommonLinks__text_main}
+                            style={{ color, paddingLeft: '8px' }}
+                        >
+                            Руководства <br />
+                            Екатеринбурга
+                        </p>
+                    </>
+                ) : (
+                    <Link
+                        style={{ paddingLeft: '12px' }}
+                        className={styles.CommonLinks__link}
+                        href="/"
+                    >
+                        <Image
+                            src="/transport.svg"
+                            width={60}
+                            height={60}
+                            className={styles.CommonLinks__icon}
+                            style={{ marginLeft: '0', marginTop: '14px' }}
+                        />
+                        <p
+                            className={styles.CommonLinks__text}
+                            style={{ color, textDecorationColor: bgColor.alpha(0.2).rgb() }}
+                        >
+                            Руководства <br />
+                            Екатеринбурга
+                        </p>
+                    </Link>
+                )}
+            </section>
+        )
     )
 }
