baf00f9a2baa44427c8c44c52a82ee58128196ba
Oleg <physic@uralweb.ru>
2023-03-29 17:59:22 +0500

    refactor Suggestions

--- components/SidePage/Suggestions/Suggestions.jsx
+++ components/SidePage/Suggestions/Suggestions.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
@@ -9,27 +9,14 @@ import styles from './Suggestions.module.css'
 import getManualColorScheme from '../../../utils/getManualColorScheme.js'
 
 const SuggestItem = ({ suggest, colorHex }) => {
-    const [origin, setOrigin] = useState('')
-
-    useEffect(() => {
-        setOrigin(window.location.origin)
-    }, [])
-
-    const { asPath } = useRouter()
-    const currentSection = asPath.split('/').filter(Boolean).at(1)
-    const link = `${origin}/${suggest?.link}`
-    const leftText = suggest?.text?.left
-    const target = suggest?.text?.target
-    const rightText = suggest?.text?.right
-
+    const {
+        link,
+        text: { left: leftText, target, right: rightText },
+    } = suggest
     const backgroundColor = getManualColorScheme(colorHex).bgLight.alpha(0.05)
-
     return (
         <li className={styles.SuggestItem__listItem} style={{ backgroundColor }}>
-            <Link
-                className={styles.SuggestItem__link}
-                href={link.includes(currentSection) ? '' : link}
-            >
+            <Link className={styles.SuggestItem__link} href={`/${link}`}>
                 <span className={styles.SuggestItem__suggestionText}>{leftText}</span>
                 <span className={styles.SuggestItem__target}>{target}</span>
                 <span className={styles.SuggestItem__suggestionText}>{rightText}</span>
@@ -39,70 +26,67 @@ const SuggestItem = ({ suggest, colorHex }) => {
 }
 
 const SectionSuggestion = ({ section, colorHex }) => {
-    const sectionName = section?.sectionName
-    const suggestions = section?.suggestions
-
+    const { sectionName, suggestions } = section
     return (
         <div>
             <p style={{ color: colorHex }} className={styles.SectionSuggestion__title}>
                 {sectionName}
             </p>
             <ul className={styles.SectionSuggestion__list}>
-                {suggestions?.map((suggest) => (
-                    <SuggestItem suggest={suggest} colorHex={colorHex} />
+                {suggestions?.map((suggest, i) => (
+                    <SuggestItem suggest={suggest} colorHex={colorHex} key={i} />
                 ))}
             </ul>
         </div>
     )
 }
 
-const GuideSuggestion = ({ guideSuggestion }) => {
-    const title = guideSuggestion?.properties.properties?.Name?.title[0]?.plain_text
-    const colorHex = guideSuggestion?.properties.properties?.color?.rich_text[0]?.plain_text
-    const icon = guideSuggestion?.properties.properties?.previewPattern?.at(0)
-    const collator = new Intl.Collator('ru', {
-        numeric: true,
-        sensitivity: 'base',
-    })
-    const sections = guideSuggestion?.sectionSuggestions.sort((a, b) =>
-        collator.compare(a.sectionName, b.sectionName)
-    )
-
+const GuideSuggestion = ({ guide }) => {
+    const { title, colorHex, icon, sections } = guide
     return (
         <article className={styles.GuideSuggestion}>
             <div className={styles.image__container}>
                 <Image className={styles.image} fill src={`${API_HOST}/static/${icon}`} alt="" />
-                <h3
-                    style={{
-                        color: colorHex,
-                    }}
-                    className={styles.GuideSuggestion__title}
-                >
+                <h3 style={{ color: colorHex }} className={styles.GuideSuggestion__title}>
                     {title}
                 </h3>
             </div>
-            {sections?.map((section) => (
-                <SectionSuggestion colorHex={colorHex} section={section} />
-            ))}
+            {sections?.map((section, i) => {
+                return <SectionSuggestion colorHex={colorHex} section={section} key={i} />
+            })}
         </article>
     )
+}
+
+function getGuides({ items, currentUrl }) {
+    const collator = new Intl.Collator('ru', {
+        numeric: true,
+        sensitivity: 'base',
+    })
+    return items.reduce((acc, item) => {
+        const title = item?.properties.properties?.Name?.title[0]?.plain_text
+        const colorHex = item?.properties.properties?.color?.rich_text[0]?.plain_text
+        const icon = item?.properties.properties?.previewPattern?.at(0)
+        const sections = item?.sectionSuggestions.sort((a, b) =>
+            collator.compare(a.sectionName, b.sectionName)
+        )
+        const guideSuggestion = { title, colorHex, icon, sections }
+        const url = item?.properties?.properties?.pageUrl?.url
+        if (url !== currentUrl) {
+            return [...acc, guideSuggestion]
+        }
+        return [guideSuggestion, ...acc]
+    }, [])
 }
 
 export const Suggestions = ({ items, query, isLoading }) => {
     const { asPath } = useRouter()
     const currentUrl = asPath.split('/').filter(Boolean).at(0)
-    const guides = items.reduce((acc, element) => {
-        const url = element?.properties?.properties?.pageUrl?.url
-        if (url !== currentUrl) {
-            return [...acc, element]
-        }
-        return [element, ...acc]
-    }, [])
-
+    const guides = getGuides({ items, currentUrl })
     return (
         <div className={styles.Suggestions__container}>
             {items.length > 0 &&
-                guides?.map((guideSuggest) => <GuideSuggestion guideSuggestion={guideSuggest} />)}
+                guides?.map((guide, i) => <GuideSuggestion guide={guide} key={i} />)}
             {items.length === 0 && query.length > 0 && !isLoading && (
                 <p className={styles.Suggestions__not_found}>Ничего не нашли</p>
             )}
