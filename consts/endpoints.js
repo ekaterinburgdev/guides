bce4eb4b39ec59580a9e39538585b62ef6320ec7
Alexander Chabin <alex.chabin@gmail.com>
2023-09-10 20:08:24 +0500

    Remove back-end endpoints from `.env`

--- consts/endpoints.js
+++ consts/endpoints.js
@@ -1,2 +1,3 @@
-export const API_HOST = process.env.NEXT_PUBLIC_HOST
+export const API_HOST = 'https://guides-api.ekaterinburg.city'
+export const NEXT_PUBLIC_IMAGE_DOMAIN = 'https://guides-api.ekaterinburg.city'
 export const PDF_HOST = `http://37.77.106.76:3000`
