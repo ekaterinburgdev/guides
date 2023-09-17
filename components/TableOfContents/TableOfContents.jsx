31c23a3ff0cd258796fe13ef8bf91b01d32b1809
atkvs <vladatkishkin@vk.com>
2023-09-17 20:44:03 +0500

    table-of-contents-scrool-into-active: alpha without intersection observer api

--- components/TableOfContents/TableOfContents.jsx
+++ components/TableOfContents/TableOfContents.jsx
@@ -46,10 +46,20 @@ function TableOfContents({ tableOfContentArr, currentPageUrl = [], anchorLinks, 
             colorScheme.bgLight.color[1]
         )}, ${Math.trunc(colorScheme.bgLight.color[2])}, ${colorScheme.bgLight.valpha})`
     )
-    const [currentActiveAnchor, setCurrentActiveAnchor] = useState(null)
 
     useEffect(() => {
         const arrayWithAnchorElements = [...document.querySelectorAll('h1[id], h2[id]')]
+
+        const isInViewport = (element) => {
+            const rect = element.getBoundingClientRect()
+
+            return (
+                rect.top >= 0 &&
+                rect.left >= 0 &&
+                rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+                rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+            )
+        }
 
         const scrollHandler = (entries) =>
             entries.forEach((entry) => {
@@ -59,7 +69,17 @@ function TableOfContents({ tableOfContentArr, currentPageUrl = [], anchorLinks, 
 
                 if (entry.intersectionRatio > 0) {
                     sectionLi?.classList?.add(styles.visible)
-                    setCurrentActiveAnchor(sectionLi)
+
+                    if (sectionLi && !isInViewport(sectionLi)) {
+                        document.addEventListener('wheel', wheelListener)
+                        function wheelListener() {
+                            sectionLi.scrollIntoView({
+                                behavior: 'smooth',
+                                block: 'start',
+                                inline: 'start',
+                            })
+                        }
+                    }
                 } else {
                     sectionLi?.classList?.remove(styles.visible)
                 }
@@ -68,33 +88,6 @@ function TableOfContents({ tableOfContentArr, currentPageUrl = [], anchorLinks, 
         const observer = new IntersectionObserver(scrollHandler)
         arrayWithAnchorElements.forEach((section) => observer.observe(section))
     }, [anchorLinks])
-
-    useEffect(() => {
-        const scrollIntoViewOptions = {
-            rootMargin: '0px',
-            threshold: 1.0,
-        }
-
-        const scrollIntoView = (entries) => {
-            entries.forEach((entry) => {
-                if (entry.intersectionRatio < 0.1) {
-                    entry.target.scrollIntoView({
-                        behavior: 'smooth',
-                        block: 'center',
-                    })
-                }
-            })
-        }
-
-        const scrollIntoViewObserver = new IntersectionObserver(
-            scrollIntoView,
-            scrollIntoViewOptions
-        )
-
-        if (currentActiveAnchor) {
-            scrollIntoViewObserver.observe(currentActiveAnchor)
-        }
-    }, [currentActiveAnchor])
 
     useEffect(() => {
         const styleVisible = document.querySelector('.visible')?.style
@@ -146,6 +139,7 @@ function TableOfContents({ tableOfContentArr, currentPageUrl = [], anchorLinks, 
                 <ul
                     style={{ backgroundColor: colorScheme.bgLight }}
                     className={styles.innerlinkContainerList}
+                    id="innerlinkContainerList"
                 >
                     {anchorLinks.map((anchor) => (
                         <li
