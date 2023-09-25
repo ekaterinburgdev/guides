1901bc1ced4baeb2b0278690e1a3b15e888e06cb
atkvs <vladatkishkin@vk.com>
2023-09-25 21:26:42 +0500

    table-of-contents-scrool-into-active: cleanup

--- components/TableOfContents/TableOfContents.jsx
+++ components/TableOfContents/TableOfContents.jsx
@@ -51,17 +51,6 @@ function TableOfContents({ tableOfContentArr, currentPageUrl = [], anchorLinks, 
     useEffect(() => {
         const arrayWithAnchorElements = [...document.querySelectorAll('h1[id], h2[id]')]
 
-        const isInViewport = (element) => {
-            const rect = element.getBoundingClientRect()
-
-            return (
-                rect.top >= 0 &&
-                rect.left >= 0 &&
-                rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-                rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-            )
-        }
-
         const scrollHandler = (entries) =>
             entries.forEach((entry) => {
                 const section = entry.target
@@ -135,7 +124,6 @@ function TableOfContents({ tableOfContentArr, currentPageUrl = [], anchorLinks, 
                 <ul
                     style={{ backgroundColor: colorScheme.bgLight }}
                     className={styles.innerlinkContainerList}
-                    id="innerlinkContainerList"
                 >
                     {anchorLinks.map((anchor) => (
                         <li
