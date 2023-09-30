3b73329741df7231c08907665cbd33722c9c12d9
Alexander Chabin <alex.chabin@gmail.com>
2023-09-30 17:26:47 +0500

    Update image lightbox styles & settings (#81)
    * Update controls styles & veil in Lightbox
    
    * Update `yet-another-react-lightbox`
    
    * Add `closeOnPullDown` option
    
    * Move variables to `ZOOM_SETTINGS`
    
    * Update zoom animation duration
    
    * Add cursor zoom in/out
    
    * Remove vertial scrollbar
    
    * Scroll to zoom true
    
    * Update pnpm-lock
    
    * Remove carousel

---
+++ consts/lightboxOptions.js
@@ -0,0 +1,30 @@
+export const ANIMATION_DURATION_SETTINGS = {
+    fade: 250,
+    zoom: 300,
+}
+
+export const LIGHTBOX_STYLES = {
+    container: {
+        background: 'white',
+    },
+    button: {
+        filter: 'none',
+        color: 'black',
+    },
+}
+
+export const CONTROLLER_SETTINGS = {
+    closeOnBackdropClick: true,
+    closeOnPullDown: true,
+}
+
+export const ZOOM_SETTINGS = {
+    doubleClickMaxStops: 3,
+    doubleTapDelay: 300,
+    keyboardMoveDistance: 50,
+    maxZoomPixelRatio: 10,
+    pinchZoomDistanceFactor: 100,
+    scrollToZoom: true,
+    wheelZoomDistanceFactor: 100,
+    zoomInMultiplier: 2,
+}
--- utils/notionTypeParser/imageParser.jsx
+++ utils/notionTypeParser/imageParser.jsx
@@ -3,22 +3,17 @@ import Image from 'next/image'
 import classNames from 'classnames'
 import Lightbox from 'yet-another-react-lightbox'
 import Zoom from 'yet-another-react-lightbox/plugins/zoom'
-import 'yet-another-react-lightbox/styles.css'
 import { API_HOST } from '../../consts/endpoints'
+
+import 'yet-another-react-lightbox/styles.css'
 import styles from '../../components/ManualPage/ManualPage.module.css'
+
 import {
-    ANIMATION_DURATION,
-    DOUBLE_CLICK_DELAY,
-    DOUBLE_CLICK_MAX_STOPS,
-    DOUBLE_TAP_DELAY,
-    IS_FINITE,
-    KEYBOARD_MOVE_DISTANCE,
-    MAX_ZOOM_PIXEL_RATIO,
-    PINCH_ZOOM_DISTANCE_FACTOR,
-    SCROLL_TO_ZOOM,
-    WHEEL_ZOOM_DISTANCE_FACTOR,
-    ZOOM_IN_MULTIPLIER,
-} from '../../consts/zoomOptions.consts'
+    ANIMATION_DURATION_SETTINGS,
+    CONTROLLER_SETTINGS,
+    LIGHTBOX_STYLES,
+    ZOOM_SETTINGS,
+} from '../../consts/lightboxOptions'
 
 function GuideImage({ notionType }) {
     const cn = classNames(styles.Manual__image, {
@@ -26,48 +21,33 @@ function GuideImage({ notionType }) {
     })
 
     const [open, setOpen] = useState(false)
-    const [animationDuration, setAnimationDuration] = useState(ANIMATION_DURATION)
-    const [maxZoomPixelRatio, setMaxZoomPixelRatio] = useState(MAX_ZOOM_PIXEL_RATIO)
-    const [zoomInMultiplier, setZoomInMultiplier] = useState(ZOOM_IN_MULTIPLIER)
-    const [doubleTapDelay, setDoubleTapDelay] = useState(DOUBLE_TAP_DELAY)
-    const [doubleClickDelay, setDoubleClickDelay] = useState(DOUBLE_CLICK_DELAY)
-    const [doubleClickMaxStops, setDoubleClickMaxStops] = useState(DOUBLE_CLICK_MAX_STOPS)
-    const [keyboardMoveDistance, setKeyboardMoveDistance] = useState(KEYBOARD_MOVE_DISTANCE)
-    const [wheelZoomDistanceFactor, setWheelZoomDistanceFactor] = useState(
-        WHEEL_ZOOM_DISTANCE_FACTOR
-    )
-    const [pinchZoomDistanceFactor, setPinchZoomDistanceFactor] = useState(
-        PINCH_ZOOM_DISTANCE_FACTOR
-    )
-    const [scrollToZoom, setScrollToZoom] = useState(SCROLL_TO_ZOOM)
-    const [finite, setFinite] = useState(IS_FINITE)
-
     const srcUrl = `${API_HOST}/static/${notionType.content.image_name}`
-
-    const zoomOptions = {
-        maxZoomPixelRatio,
-        zoomInMultiplier,
-        doubleTapDelay,
-        doubleClickDelay,
-        doubleClickMaxStops,
-        keyboardMoveDistance,
-        wheelZoomDistanceFactor,
-        pinchZoomDistanceFactor,
-        scrollToZoom,
-    }
 
     const image = (
         <>
+            {/* Add zoom-in / zoom-out cursors */}
+            <style
+                dangerouslySetInnerHTML={{
+                    __html: `
+                    .yarl__root:has([aria-label="Zoom out"]:not(:disabled)) .yarl__slide_image {
+                        cursor: zoom-out;
+                    }
+
+                    .yarl__root:has([aria-label="Zoom in"]:not(:disabled)) .yarl__slide_image {
+                        cursor: zoom-in;
+                    }
+            `,
+                }}
+            />
             <Lightbox
                 open={open}
                 close={() => setOpen(false)}
                 slides={[{ src: srcUrl }]}
                 plugins={[Zoom]}
-                animation={{ zoom: animationDuration }}
-                zoom={zoomOptions}
-                carousel={{
-                    finite,
-                }}
+                zoom={ZOOM_SETTINGS}
+                animation={ANIMATION_DURATION_SETTINGS}
+                controller={CONTROLLER_SETTINGS}
+                styles={LIGHTBOX_STYLES}
                 render={{
                     buttonPrev: () => null,
                     buttonNext: () => null,
