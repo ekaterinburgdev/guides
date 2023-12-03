6856493d1d7601950b8f7b13908495be300d2d2d
Alexander Chabin <alex.chabin@gmail.com>
2023-12-03 20:29:12 +0500

    Add color to `<Callout />` (#98)
    * Add color to `<Callout />`
    
    * Fix ESLint errors
    
    * Remove useless style="color: *"

--- components/NotionTypes/Callout/Callout.jsx
+++ components/NotionTypes/Callout/Callout.jsx
@@ -4,15 +4,17 @@ import Image from 'next/image'
 import { getTextContent } from '../../../utils/textParser'
 
 import styles from './Callout.module.css'
+import { getNotionColor } from '../../../utils/colors'
 
 export const Callout = ({ columnItem }) => {
     const iconUrl = columnItem?.content?.icon?.external?.url
     const text = getTextContent(columnItem, true)
+    const color = getNotionColor(columnItem?.content?.color)
 
     return (
         <div className={styles.Callout__container}>
             <Image className={styles.Callout__icon} width={25} height={25} src={iconUrl} alt="" />
-            <div>{text}</div>
+            <div style={{ color }}>{text}</div>
         </div>
     )
 }
