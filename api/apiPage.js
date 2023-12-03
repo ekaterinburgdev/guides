9b16e4e9bfdc64cbf227f61bade66caaa747cc8d
Alexander Chabin <nibach@ya.ru>
2023-12-03 20:27:33 +0500

    Fix apiPage ESLint

--- api/apiPage.js
+++ api/apiPage.js
@@ -1,35 +1,35 @@
-import { API_HOST } from "../consts/endpoints";
+import { API_HOST } from '../consts/endpoints'
 
 /** @returns { Promise<import('./apiPage').Root> } */
 export async function getPage(pageId) {
-    const url = new URL(`${API_HOST}/api/content/root`);
-    url.searchParams.append("id", pageId);
+    const url = new URL(`${API_HOST}/api/content/root`)
+    url.searchParams.append('id', pageId)
 
-    const resp = await fetch(url);
+    const resp = await fetch(url)
 
-    return await resp.json();
+    return await resp.json()
 }
 
 /** @returns { Promise<import('./apiPage').AllPage> } */
 export async function getAllPage() {
-    const url = new URL(`${API_HOST}/api/options`);
-    const resp = await fetch(url);
+    const url = new URL(`${API_HOST}/api/options`)
+    const resp = await fetch(url)
 
-    return await resp.json();
+    return await resp.json()
 }
 
 /** @returns { Promise<import('./apiPage').Tree> } */
 export async function getTree() {
-    const url = new URL(`${API_HOST}/api/tree`);
-    const resp = await fetch(url);
+    const url = new URL(`${API_HOST}/api/tree`)
+    const resp = await fetch(url)
 
-    return await resp.json();
+    return await resp.json()
 }
 
 /** @returns { Promise<import('./apiPage').Page> } */
 export async function getPageByUrl(path) {
-    const url = new URL(`${API_HOST}/api/content/root/${path}`);
-    const resp = await fetch(url);
+    const url = new URL(`${API_HOST}/api/content/root/${path}`)
+    const resp = await fetch(url)
 
-    return await resp.json();
+    return await resp.json()
 }
