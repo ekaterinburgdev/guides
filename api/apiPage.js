2ec071d302b592763c75726658e1a97b361cfb7a
Alexander Chabin <nibach@ya.ru>
2023-12-04 22:03:08 +0500

    Remove unused `getPage` & `getAllPage` backend methods

--- api/apiPage.js
+++ api/apiPage.js
@@ -1,23 +1,5 @@
 import { API_HOST } from '../consts/endpoints'
 
-/** @returns { Promise<import('./apiPage').Root> } */
-export async function getPage(pageId) {
-    const url = new URL(`${API_HOST}/api/content/root`)
-    url.searchParams.append('id', pageId)
-
-    const resp = await fetch(url)
-
-    return await resp.json()
-}
-
-/** @returns { Promise<import('./apiPage').AllPage> } */
-export async function getAllPage() {
-    const url = new URL(`${API_HOST}/api/options`)
-    const resp = await fetch(url)
-
-    return await resp.json()
-}
-
 /** @returns { Promise<import('./apiPage').Tree> } */
 export async function getTree() {
     const url = new URL(`${API_HOST}/api/tree`)
