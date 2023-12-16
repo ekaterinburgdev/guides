4a86882d50504d8feef6f71ffd7fa185e765682d
Alexander Chabin <nibach@ya.ru>
2023-12-16 06:37:24 +0500

    Add SVGR for inline svg in React

--- next.config.js
+++ next.config.js
@@ -2,6 +2,29 @@ import { NEXT_PUBLIC_IMAGE_DOMAIN } from './consts/endpoints.js';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    webpack(config) {
+        const fileLoaderRule = config.module.rules.find((rule) =>
+            rule.test?.test?.('.svg'),
+        )
+
+        config.module.rules.push(
+            {
+                ...fileLoaderRule,
+                test: /\.svg$/i,
+                resourceQuery: /url/,
+            },
+            {
+                test: /\.svg$/i,
+                issuer: fileLoaderRule.issuer,
+                resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
+                use: ['@svgr/webpack'],
+            },
+        )
+
+        fileLoaderRule.exclude = /\.svg$/i
+
+        return config
+    },
     images: {
         domains: [NEXT_PUBLIC_IMAGE_DOMAIN],
         unoptimized: true,
