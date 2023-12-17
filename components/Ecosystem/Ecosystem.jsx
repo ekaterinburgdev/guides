a214a2b6b07c5cf7307af34810e80df4ec59cc7f
Alexander Chabin <nibach@ya.ru>
2023-12-17 09:30:49 +0500

    Remove guide color for Ecosystem

--- components/Ecosystem/Ecosystem.jsx
+++ components/Ecosystem/Ecosystem.jsx
@@ -13,7 +13,6 @@ export function Ecosystem() {
                 toggleStyle={{
                     backgroundColor: `var(--ecosystem-color)`,
                     width: 'var(--ecosystem-width)',
-                    '--projects-panel-text-color': 'var(--color)',
                 }}
                 style={{
                     left: '8px',
