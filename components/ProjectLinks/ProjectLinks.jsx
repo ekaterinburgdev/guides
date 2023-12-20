b04fba8f1ced31c321a5e576a7176230a3c759dc
Alexander Chabin <nibach@ya.ru>
2023-12-20 09:15:18 +0500

    Move button to separate component

--- components/ProjectLinks/ProjectLinks.jsx
+++ components/ProjectLinks/ProjectLinks.jsx
@@ -1,28 +1,27 @@
 import React from 'react'
-
 import cn from 'classnames'
+import { Button } from '../Button/Button'
+import Github from './github.svg'
 
 import styles from './ProjectLinks.module.css'
-
-import Github from './github.svg'
 
 export function ProjectLinks() {
     return (
         <div className={cn(styles.ProjectLinks)}>
             <a
-                className={cn(styles.ProjectLinks__button, styles.ProjectLinks__button_about)}
+                className={cn(styles.ProjectLinks__github)}
                 href="https://github.com/ekaterinburgdev/guides"
                 target="_blank"
                 aria-label="Код проекта на GitHub"
             >
                 <Github />
             </a>
-            <a
-                className={cn(styles.ProjectLinks__button, styles.ProjectLinks__button_feedback)}
-                href="https://tally.so#tally-open=w5ZYXd&tally-width=650&tally-overlay=1&tally-emoji-animation=none"
+            <Button
+                type="secondary"
+                link="https://tally.so#tally-open=w5ZYXd&tally-width=650&tally-overlay=1&tally-emoji-animation=none"
             >
                 Фидбек
-            </a>
+            </Button>
         </div>
     )
 }
