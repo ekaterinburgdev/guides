99a1a0b22cc7f38db3493734188e246a8cc967b4
Alexander Chabin <nibach@ya.ru>
2023-12-21 10:46:09 +0500

    `setIsOpen(false)` when click on anchors

--- components/TableOfContents/TableOfContents.jsx
+++ components/TableOfContents/TableOfContents.jsx
@@ -8,13 +8,13 @@ import { ManualTitle } from '../ManualTitle/ManualTitle'
 
 import styles from './TableOfContents.module.css'
 
-function InnerLink({ anchor, baseState, setState }) {
+function InnerLink({ anchor, setIsOpen }) {
     return (
         <a
             className={cn(styles.innerTableOfContentsLink)}
             key={anchor.title}
             href={`#${anchor.id}`}
-            onClick={() => setState(!baseState)}
+            onClick={() => setIsOpen(false)}
         >
             <span>{tUI(anchor.title[0])}</span>
         </a>
@@ -87,7 +87,7 @@ function TableOfContents({
                                 <InnerLink
                                     anchor={anchor}
                                     baseState={isOpen}
-                                    setState={setIsOpen}
+                                    setIsOpen={setIsOpen}
                                 />
                             </li>
                         ))}
