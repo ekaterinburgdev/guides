c37ef48795d021e594af3d204e4de3b99de5cc1c
Alexander Chabin <nibach@ya.ru>
2023-12-21 10:16:32 +0500

    Fix const values in `--color-s` / `--color-l`

--- utils/getCSSVarsColors.js
+++ utils/getCSSVarsColors.js
@@ -1,13 +1,13 @@
 import { hexToHSL } from './hexToHSL'
 
 export function getCSSVarsColors(color) {
-    const { h, s, l } = hexToHSL(color)
+    const { h } = hexToHSL(color)
 
     return {
         '--color-h': h,
-        '--color-s': s,
-        '--color-l': l,
-        '--color-hsl': `var(--color-h) 45% 45%`,
+        '--color-s': '45%',
+        '--color-l': '45%',
+        '--color-hsl': `var(--color-h) var(--color-s) var(--color-l)`,
         '--color': `hsl(var(--color-hsl))`,
     }
 }
